f366f85470f260ca45c7cb6e67bb3bf41e893918
Aleksey <v207vv@gmail.com>
2026-07-24 22:03:43 +0500

    1commit

---
+++ main/models.py
@@ -0,0 +1,59 @@
+from django.core.management import color
+from django.db import models
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100,unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Category,
+                                on_delete=models.CASCADE,
+                                related_name='product_sizes'
+                                )
+
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return F"{self.size} ({self.stock} in stock ) for {self.product}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100,unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='products')
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    main_image = models.ImageField(upload_to='products/main/')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='images')
+    image = models.ImageField(upload_to='products/extra/')
